rciam/simplesamlphp-module-themevanilla

Add tab title in translation

themes/ssp/default/includes/header.php
@@ -48,7 +48,11 @@ header('X-Frame-Options: SAMEORIGIN');
 <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0" />
 <script type="text/javascript" src="/<?php echo $this->data['baseurlpath']; ?>resources/script.js"></script>
 <title>
-  SimpleSAMLphp
+  <?php
+  if (strpos($this->t('{themevanilla:default:browser_tab_title}'), 'not translated') === FALSE) {
+    echo $this->t('{themevanilla:default:browser_tab_title}');
+  }
+  ?>
   <?php if(array_key_exists('header', $this->data)) { echo (' | ' . $this->data['header']); } ?>
 </title>
 

EGI-Federation/simplesamlphp-module-themeegi

Conflicts:
	themes/ssp/default/includes/header.php

themes/ssp/default/includes/header.php
@@ -47,8 +47,12 @@ header('X-Frame-Options: SAMEORIGIN');
 <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0" />
 <script type="text/javascript" src="/<?php echo $this->data['baseurlpath']; ?>resources/script.js"></script>
 <title>
-  EGI
-  <?php if(array_key_exists('header', $this->data)) { echo (' | ' . $this->data['header']); } ?>
+  <?php
+  if (strpos($this->t('{themeegi:default:browser_tab_title}'), 'not translated') === FALSE) {
+    echo $this->t('{themeegi:default:browser_tab_title}');
+  }
+  if(array_key_exists('header', $this->data)) { echo (' | ' . $this->data['header']); } 
+  ?>
 </title>
 
 <link rel="stylesheet" type="text/css" href="<?php echo htmlspecialchars(SimpleSAML_Module::getModuleURL('themeegi/resources/css/app.css')); ?>" />