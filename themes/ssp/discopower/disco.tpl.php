283d13ff250deecc54ffd68dfc59056909faf655
nikosev <nikos.ev@hotmail.com>
2021-04-05 11:23:23 +0300

    Update translation handling

--- themes/ssp/discopower/disco.tpl.php
+++ themes/ssp/discopower/disco.tpl.php
@@ -1,4 +1,7 @@
 <?php
+
+use Webmozart\Assert\Assert;
+
 // Get Configuration and set the loader
 $themeConfig = SimpleSAML_Configuration::getConfig('module_themeegi.php');
 $loader = $themeConfig->getValue('loader');
@@ -104,15 +107,15 @@ function showEntry($t, $metadata, $favourite = FALSE, $withIcon = FALSE) {
 function getTranslatedName($t, $metadata) {
   if (isset($metadata['UIInfo']['DisplayName'])) {
     $displayName = $metadata['UIInfo']['DisplayName'];
-    assert('is_array($displayName)'); // Should always be an array of language code -> translation
+    Assert::isArray($displayName); // Should always be an array of language code -> translation
     if (!empty($displayName)) {
-      return $t->getTranslation($displayName);
+      return $t->getTranslator()->getPreferredTranslation($displayName);
     }
   }
 
   if (array_key_exists('name', $metadata)) {
     if (is_array($metadata['name'])) {
-      return $t->getTranslation($metadata['name']);
+      return $t->getTranslator()->getPreferredTranslation($metadata['name']);
     } else {
       return $metadata['name'];
     }
