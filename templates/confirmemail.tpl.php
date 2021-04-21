b5af66a91887b9f7c09b10a8040dc80f6ef66e45
nikosev <nikos.ev@hotmail.com>
2021-04-21 11:21:41 +0300

    Fix identation of HTML

--- templates/confirmemail.tpl.php
+++ templates/confirmemail.tpl.php
@@ -5,12 +5,12 @@ $this->includeAtTemplateBase('includes/header.php');
 
 <div class="body-container" id="content">
     <div class="message-container">
-      <span class="head-title">
-        <img class="email-gif" src="resources/icons/email.gif">
-        <h2><span class="title-msg"><?= $this->t('{themeegi:default:message_title}') ?></span></h2>
-      </span>
-      <div class="message"><?= $this->t('{themeegi:default:message_bd}') ?></div>
-      <div class="subtitle"><?= $this->t('{themeegi:default:message_sub}') ?></div>
+        <span class="head-title">
+            <img class="email-gif" src="resources/icons/email.gif">
+            <h2><span class="title-msg"><?= $this->t('{themeegi:default:message_title}') ?></span></h2>
+        </span>
+        <div class="message"><?= $this->t('{themeegi:default:message_bd}') ?></div>
+        <div class="subtitle"><?= $this->t('{themeegi:default:message_sub}') ?></div>
     </div>
 </div>
 
